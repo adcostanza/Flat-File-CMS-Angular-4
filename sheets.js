931dadc703063f205deb28572fa3e554ef9d5d1c
Adam <adcostanza@gmail.com>
2017-11-17 18:51:46 -0700

    CMS part working, time to add angular

---
+++ sheets.js
@@ -0,0 +1,33 @@
+function DatatoJSON() {
+  var _json = [];
+  var ss=SpreadsheetApp.getActiveSpreadsheet();
+    var sheet=ss.getSheets()[0];
+    var data = sheet.getDataRange().getValues();
+  //Logger.log(data);
+  for(var r = 1; r<data.length;r++) {
+    var element = {}
+    for(var c = 0; c<data[0].length;c++) {
+      element[data[0][c]] = data[r][c];
+    }
+    _json.push(element);
+  }
+  Logger.log(JSON.stringify(_json));
+  return JSON.stringify(_json);
+}
+
+function sendData() {
+  var query = DatatoJSON();
+    var url = 'http://acostanza.com/php-cms/writeMenu.php?key=rQLFqUYa8LnwcIH0TI83fO4g3bnFWEMfELLOvl';
+  var options = {
+   'method' : 'post',
+   'payload' : query
+ };
+    var response = UrlFetchApp.fetch(url,options);
+  Logger.log(response);
+
+  if(response == "success") {
+     SpreadsheetApp.getUi().alert('Successfully updated content.');
+  } else {
+    SpreadsheetApp.getUi().alert('Failed to update content.');
+  }
+}
--- writeMenu.php
+++ writeMenu.php
@@ -1,6 +1,6 @@
 <?php
-if($_GET['key']=='rQLFqUYa8LnwcIHqR47R68tzwFeIrkZoKk5mj0TI83fO4g3bnFWEMfELLOvl') {
-  file_put_contents('menu.json', stripslashes(file_get_contents('php://input')));
+if($_GET['key']=='rQLFqUYa8LnwcIHqR47R68tg3bnFWEMfELLOvl') {
+  file_put_contents('menu.json', file_get_contents('php://input'));
   echo "success";
 } else {
   echo "Unauthorized";
